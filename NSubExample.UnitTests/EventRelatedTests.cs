c73750fc7c01015475dca6cb199da134c40fc5d7
Branko Rados <branko.rados@fer.hr>
2019-10-18 16:02:08 +0200

    Testing an event listener

---
+++ NSubExample.UnitTests/EventRelatedTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUintExample;
+
+namespace NSubExample.UnitTests
+{
+    [TestFixture]
+    public class EventRelatedTests
+    {
+        [Test]
+        public void ctor_WhenViewIsLoaded_CallsViewRender()
+        {
+            var mockView = Substitute.For<IView>();
+
+            Presenter p = new Presenter(mockView);
+            mockView.Loaded += Raise.Event<Action>();
+
+            mockView.Received().Render(Arg.Is<string>(s => s.Contains("Hello World")));
+        }
+    }
+}
