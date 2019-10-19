1c420281da939e4a197aaf85f94904fbb71c164e
Branko Rados <branko.rados@fer.hr>
2019-10-19 20:21:05 +0200

    Recursive fakes

---
+++ NSubExample.UnitTests/PersonTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUintExample;
+using NSubstitute;
+
+namespace NSubExample.UnitTests
+{
+    [TestFixture]
+    public class PersonTests
+    {
+        [Test]
+        public void RecursiveFakes_work()
+        {
+            IPerson p = Substitute.For<IPerson>();
+
+            Assert.IsNotNull(p.GetManager());
+            Assert.IsNotNull(p.GetManager().GetManager());
+            Assert.IsNotNull(p.GetManager().GetManager());
+        }
+      
+    }
+}
