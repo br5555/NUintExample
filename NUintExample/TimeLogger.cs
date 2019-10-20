e6e29a51bbca15624208f89aa48eab711f31ca4e
Branko Rados <branko.rados@fer.hr>
2019-10-20 10:53:49 +0200

    Cross-cutting corners injection

---
+++ NUintExample/TimeLogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUintExample
+{
+    public static class TimeLogger
+    {
+        public static string CreateMessage(string info)
+        {
+            //Production code that uses SystemTime
+            return SystemTime.Now.ToShortDateString() + " " + info;
+        }
+    }
+}
---
+++ NUnitExample.UnitTest/TimeLoggerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUintExample;
+
+namespace NUnitExample.UnitTest
+{
+    [TestFixture]
+    public class TimeLoggerTests
+    {
+
+        public void SettingSystemTime_Always_ChangeTime()
+        {
+            //set fake date
+            SystemTime.Set(new DateTime(2000, 1, 1));
+
+            string output = TimeLogger.CreateMessage("a branko");
+
+            StringAssert.Contains("01.01.2000", output);
+        }
+
+        [TearDown]
+        public void afterEachTest()
+        {
+            //Reset date at end of each test
+            SystemTime.Reset();
+        }
+    }
+}
